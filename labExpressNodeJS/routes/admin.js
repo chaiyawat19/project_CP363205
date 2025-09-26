b6c86fa5c284a7c1f72fc1101c1614ac4cf4ce2d
cheetahzaza555 <cheetahzaza555@gmail.com>
2025-09-26 22:16:27 +0700

    can logout

--- labExpressNodeJS/routes/admin.js
+++ labExpressNodeJS/routes/admin.js
@@ -6,4 +6,15 @@ router.get('/', isAdmin, (req, res) => {
   res.render('indexAdmin', { title: 'หน้าหลัก Admin', name: req.session.userName , layout: 'layouts/navadmin'});
 });
 
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error(err);
+      return res.redirect('/');
+    }
+    res.clearCookie('connect.sid'); // ลบ cookie ออกด้วย
+    res.redirect('/'); // กลับไปหน้า login หรือหน้าแรก
+  });
+});
+
 module.exports = router;
--- labExpressNodeJS/routes/users.js
+++ labExpressNodeJS/routes/users.js
@@ -6,4 +6,15 @@ router.get('/', isUser, (req, res) => {
   res.render('indexUser', { title: 'หน้าหลัก User', name: req.session.userName,layout: 'layouts/navuser'});
 });
 
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error(err);
+      return res.redirect('/');
+    }
+    res.clearCookie('connect.sid'); // ลบ cookie ออกด้วย
+    res.redirect('/'); // กลับไปหน้า login หรือหน้าแรก
+  });
+});
+
 module.exports = router;
