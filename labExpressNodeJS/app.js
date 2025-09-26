ecd500cd1516e3f87497c3cb0f6f5536f6fcd8a3
cheetahzaza555 <cheetahzaza555@gmail.com>
2025-09-26 21:53:26 +0700

    fig err

--- labExpressNodeJS/app.js
+++ labExpressNodeJS/app.js
@@ -20,7 +20,7 @@ const PORT = 3001;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
-app.set('layout', 'layouts/main'); 
+app.set('layout', 'layouts/index'); 
 app.set('view engine', 'ejs');
 
 // middleware
