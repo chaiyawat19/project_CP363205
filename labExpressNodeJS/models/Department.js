cdec2d89067a1b65d344542feaeec6ec7f487066
nnzzy <diagito1156@gmail.com>
2025-10-10 20:26:58 +0700

    20:26

--- labExpressNodeJS/models/Department.js
+++ labExpressNodeJS/models/Department.js
@@ -1,24 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-const DepartmentSchema = new mongoose.Schema({
-    // ชื่อแผนก (จำเป็นต้องมี, ไม่ซ้ำกัน)
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    deleted_at: {
-        type: Date,
-        default: null
-    }
-}, {
-    timestamps: true
-});
-
-module.exports = mongoose.model('Department', DepartmentSchema);
-=======
 const departmentSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -35,7 +16,5 @@ const departmentSchema = new mongoose.Schema({
     default: Date.now
   }
 });
-
 module.exports = mongoose.model('Department', departmentSchema);
 
->>>>>>> a9001332d64f37c5d1ac1730fedcbc626f33ade6
